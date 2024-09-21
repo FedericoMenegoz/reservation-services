fa51ab29aa8df412dae7d988622c66895934bc5e
Federico Menegoz <menegoz.federico@gmail.com>
2024-09-21 10:19:51 +0200

    Just to check the pipeline

---
+++ src/test/java/com/cherry/fm/reservationservices/services/ReservationServiceTest.kt
@@ -0,0 +1,16 @@
+package com.cherry.fm.reservationservices.services
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+class ReservationServiceTest {
+
+    @Test
+    fun saveReservation() {
+    }
+
+    @Test
+    fun getReservationById() {
+    }
+}
